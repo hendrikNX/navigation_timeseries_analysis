0acee93debdb9642ba4850d1f1cdc7ddae457404
Hendrik Niemax <2410837670@stud.fh-kufstein.ac.at>
2025-05-17 20:53:39 +0200

    Add check to RouteData model to ensure correct time format and add tests for API #22

--- src/data_models.py
+++ src/data_models.py
@@ -13,5 +13,11 @@ class RouteData:
     duration_in_traffic_s: int
     start_time: datetime # Should be timezone-aware
 
+    def __post_init__(self):
+        if not isinstance(self.start_time, datetime):
+            raise TypeError(f"start_time must be a datetime object, but got {type(self.start_time)}.")
+        if self.start_time.tzinfo is None or self.start_time.tzinfo.utcoffset(self.start_time) is None:
+            raise ValueError("start_time must be timezone-aware.")
+
     def get_field_names(self) -> List[str]:
         return [field.name for field in fields(self)]
